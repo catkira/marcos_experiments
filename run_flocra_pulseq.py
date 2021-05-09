a79a7442922bfe6c750b03a07e835b06df079f6c
Benjamin Menküc <benjamin.menkuec@fh-dortmund.de>
2021-05-09 22:12:55 +0200

    general purpose pulseq flocra executor

---
+++ run_flocra_pulseq.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+import time
+import argparse
+from mri_config import lo_freq, grad_max_Hz_per_m, hf_max_Hz_per_m, gamma
+
+import external
+import experiment as ex
+import os
+import scipy.io as sio
+from shutil import copyfile
+from flocra_pulseq_interpreter import PSInterpreter
+st = pdb.set_trace
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run a pulseq sequence on FLOCRA')
+    parser.add_argument('seq', metavar='seq', type=str, nargs='+',
+                    help='pulseq generated .seq file')
+    args = parser.parse_args()                    
+    print(F"seq file: {args.seq}")
+
+    tx_t = 1 # us
+    grad_t = 10 #
+
+    print('gradient max_B_per_m = {:f} mT/m'.format(grad_max_Hz_per_m/gamma*1e3))	
+    print('gradient max_Hz_per_m = {:f} MHz/m'.format(grad_max_Hz_per_m/1E6))
+    print('HF max_Hz_per_m = {:f} kHz'.format(hf_max_Hz_per_m/1E3))
+
+    seq_file = args.seq[0]
+    psi = PSInterpreter(rf_center=lo_freq*1e6,
+                        rf_amp_max=hf_max_Hz_per_m,
+                        tx_t=tx_t,
+                        grad_t=grad_t,
+                        grad_max=grad_max_Hz_per_m)
+    od, pd = psi.interpret(seq_file)   
+
+    TR = pd['TR']
+    Nx = pd['Nx']
+    sliceThickness = int(pd['SliceThickness'])
+
+    from datetime import datetime
+    now = datetime.now()
+    current_time = now.strftime("%y-%m-%d %H_%M_%S")
+    if seq_file.find("radial") != -1:
+        Nspokes = int(pd['Nspokes'])
+        angle = int(pd['angle'])
+        filename = f"{seq_file[:-4]} {current_time} Nspokes {Nspokes} Nx {Nx} TR {TR} angle {angle} SliceThickness {sliceThickness}"
+    else:
+        Ny = int(pd['Ny'])
+        filename = f"{seq_file[:-4]} {current_time} Ny {Ny} Nx {Nx} TR {TR} SliceThickness {sliceThickness}"
+    copyfile(seq_file,filename+".seq")        
+
+    expt = ex.Experiment(lo_freq=lo_freq,
+                         rx_t=pd['rx_t'],
+                         init_gpa=True,
+                         gpa_fhdo_offset_time=grad_t/3) 
+    expt.add_flodict(od)
+
+    expt.gradb.calibrate(channels=[0,1,2], max_current=max_grad_current, num_calibration_points=30, averages=5, poly_degree=5)
+
+    rxd, msgs = expt.run()
+    nSamples = pd['readout_number']
+    
+    np.save(filename + ".npy",rxd['rx0'])
+    sio.savemat(filename + ".mat",rxd)
+    plt.close()
+    plt.ioff()
