9131ea18825bc7c8bcedbc398cb20f1a36fedeca
Benjamin Menkuec <benjamin@menkuec.de>
2021-05-24 14:10:14 +0200

    fix

--- tabletop_noise_monitor.py
+++ tabletop_noise_monitor.py
@@ -7,6 +7,7 @@ import pdb
 import external
 import experiment as ex
 import os
+import time
 #from pulseq_assembler import PSAssembler
 from flocra_pulseq_interpreter import PSInterpreter
 st = pdb.set_trace
@@ -15,6 +16,7 @@ from mri_config import lo_freq, grad_max_Hz_per_m, hf_max_Hz_per_m, gamma, shim
 if __name__ == "__main__":  
     print('HF max_Hz_per_m = {:f} kHz'.format(hf_max_Hz_per_m/1E3))
 
+    grad_max = grad_max_Hz_per_m # factor used to normalize gradient amplitude, should be max value of the gpa used!	
     rf_amp_max = hf_max_Hz_per_m # factor used to normalize RF amplitude, should be max value of system used!
     psi = PSInterpreter(rf_center=lo_freq*1e6,
                         rf_amp_max=rf_amp_max,
@@ -33,16 +35,19 @@ if __name__ == "__main__":
     expt.gradb.calibrate(channels=[0,1,2], max_current=6, num_calibration_points=30, averages=5, poly_degree=5)
 
     fig, (ax1, ax2, ax3) = plt.subplots(3)
-    dt = pd['rx_t']
-    t_axis = np.linspace(0, dt * nSamples, nSamples)  # us    
     while True:
         rxd, msgs = expt.run()
         data = rxd['rx0']
         data = data[6:]
         nSamples_orig = pd['readout_number']   
         nSamples = len(data)
+        dt = pd['rx_t']
+        t_axis = np.linspace(0, dt * nSamples, nSamples)  # us    
         noise = np.abs(np.std(np.real(np.fft.fft(data))))
         fig.suptitle('Noise Monitor [n={:d}, lo_freq={:f} Mhz]\nNoise={:f}'.format(nSamples_orig,lo_freq,noise))
+        ax1.clear()
+        ax2.clear()
+        ax3.clear()
         ax1.plot(t_axis, np.abs(data)*3.3)
         ax1.set_ylabel('abs [mV]')
         ax2.set_xlabel('time [us]')
@@ -54,8 +59,11 @@ if __name__ == "__main__":
         #ax3.plot(f_axis,np.abs(np.fft.fftshift(np.fft.fft(data))[int(nSamples/2)-nFFT_window:int(nSamples/2)+nFFT_window]/np.sqrt(nSamples)))
         f_axis = np.fft.fftshift(np.fft.fftfreq(nSamples,dt*1E-6))
         ax3.plot(f_axis,np.abs(np.fft.fftshift(np.fft.fft(data))/np.sqrt(nSamples)))
+        plt.ion()
         plt.show()
+        plt.pause(0.001)        
         fig.tight_layout()
+        time.sleep(1)
     expt.gradb.init_hw()  # set gradient currents back to zero
     expt.close_server(True) 
     # st()    
