9bb8f6a291d99cfd6540ed65f51e17c39a1d344e
Benjamin Menküc <benjamin.menkuec@fh-dortmund.de>
2021-05-24 09:14:53 +0200

    add noise monitor

---
+++ tabletop_noise_monitor.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+
+import external
+import experiment as ex
+import os
+#from pulseq_assembler import PSAssembler
+from flocra_pulseq_interpreter import PSInterpreter
+st = pdb.set_trace
+from mri_config import lo_freq, grad_max_Hz_per_m, hf_max_Hz_per_m, gamma, shim
+
+if __name__ == "__main__":  
+    print('HF max_Hz_per_m = {:f} kHz'.format(hf_max_Hz_per_m/1E3))
+
+    rf_amp_max = hf_max_Hz_per_m # factor used to normalize RF amplitude, should be max value of system used!
+    psi = PSInterpreter(rf_center=lo_freq*1e6,
+                        rf_amp_max=rf_amp_max,
+                        grad_max=grad_max) # very large, just for testing
+    od, pd = psi.interpret("tabletop_se_pulseq.seq")         
+    grad_interval = pd['grad_t']
+
+
+    expt = ex.Experiment(lo_freq=lo_freq,
+                         rx_t=pd['rx_t'],
+                         init_gpa=True,
+                         gpa_fhdo_offset_time=grad_interval/3,
+                         flush_old_rx=True,
+                         halt_and_reset=True) 
+    expt.add_flodict(od)
+    expt.gradb.calibrate(channels=[0,1,2], max_current=6, num_calibration_points=30, averages=5, poly_degree=5)
+
+    fig, (ax1, ax2, ax3) = plt.subplots(3)
+    dt = pd['rx_t']
+    t_axis = np.linspace(0, dt * nSamples, nSamples)  # us    
+    while True:
+        rxd, msgs = expt.run()
+        data = rxd['rx0']
+        data = data[6:]
+        nSamples_orig = pd['readout_number']   
+        nSamples = len(data)
+        noise = np.abs(np.std(np.real(np.fft.fft(data))))
+        fig.suptitle('Noise Monitor [n={:d}, lo_freq={:f} Mhz]\nNoise={:f}'.format(nSamples_orig,lo_freq,noise))
+        ax1.plot(t_axis, np.abs(data)*3.3)
+        ax1.set_ylabel('abs [mV]')
+        ax2.set_xlabel('time [us]')
+        ax2.plot(t_axis, data.real*3.3)
+        ax2.set_ylabel('real [mV]')
+        #f_axis = np.linspace(-1/dt*nSamples,1/dt*nSamples,nSamples)
+        #nFFT_window = 127
+        #f_axis = np.fft.fftshift(np.fft.fftfreq(nSamples,dt*1E-6))[int(nSamples/2)-nFFT_window:int(nSamples/2)+nFFT_window]
+        #ax3.plot(f_axis,np.abs(np.fft.fftshift(np.fft.fft(data))[int(nSamples/2)-nFFT_window:int(nSamples/2)+nFFT_window]/np.sqrt(nSamples)))
+        f_axis = np.fft.fftshift(np.fft.fftfreq(nSamples,dt*1E-6))
+        ax3.plot(f_axis,np.abs(np.fft.fftshift(np.fft.fft(data))/np.sqrt(nSamples)))
+        plt.show()
+        fig.tight_layout()
+    expt.gradb.init_hw()  # set gradient currents back to zero
+    expt.close_server(True) 
+    # st()    
+
+  
+
+ 
